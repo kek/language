28ebe5dc22b33dcc3d26a155c4eb6d57ebc9f2ab
Karl Eklund <localpart@gmail.com>
2019-04-28 16:33:32 +0200

    Extract libraries for tests into Symbelix.TestHelpers.Libraries

--- lib/symbelix.ex
+++ lib/symbelix.ex
@@ -49,7 +49,7 @@ defmodule Symbelix do
   ## Examples:
 
       iex> compile([{:atom, 1, 'add'}, {:number, 1, 1}, {:number, 1, 2}], Mathematician)
-      {:ok, {:apply, [context: Symbelix.Library, import: Kernel], [SymbelixTest.Mathematician, :add, [1, 2]]}}
+      {:ok, {:apply, [context: Symbelix.Library, import: Kernel], [Symbelix.TestHelpers.Libraries.Mathematician, :add, [1, 2]]}}
 
       iex> compile([{:atom, 1, 'aliens'}, {:atom, 1, 'built'}, {:atom, 1, 'it'}], Mathematician)
       {:error, "Unknown function (atom) 'aliens' at line 1 with 2 parameter(s): (built it)"}
@@ -58,7 +58,7 @@ defmodule Symbelix do
       {:error, "The module PHP doesn't exist"}
 
       iex> compile([{:atom, 1, 'public'}], Java)
-      {:error, "The module SymbelixTest.Java doesn't implement Symbelix.Library behaviour"}
+      {:error, "The module Symbelix.TestHelpers.Libraries.Java doesn't implement Symbelix.Library behaviour"}
 
       iex> compile([{:atom, 1, 'proc'}, {:atom, 1, 'add'}, {:number, 1, 1}, {:number, 1, 2}], Mathematician)
       {:ok, {:proc, [{:atom, 1, 'add'}, {:number, 1, 1}, {:number, 1, 2}]}}
--- test/symbelix_test.exs
+++ test/symbelix_test.exs
@@ -1,74 +1,16 @@
 defmodule SymbelixTest do
   use ExUnit.Case
   import Symbelix
-  alias SymbelixTest.Mathematician
 
-  defmodule Mathematician do
-    use Symbelix.Library
-
-    def add(a, b), do: a + b
-    def add(a, b, c), do: a + b + c
-  end
-
-  defmodule Controller do
-    use Symbelix.Library
-
-    def if('true', yes, _), do: yes
-    def if('false', _, no), do: no
-
-    def inc(n), do: n + 1
-  end
-
-  defmodule Talker do
-    use Symbelix.Library
-
-    def say(string), do: string
-  end
-
-  defmodule ListProcessor do
-    use Symbelix.Library
-
-    def identity(x), do: x
-    def first([head | _]), do: head
-  end
-
-  defmodule Java do
-  end
-
-  defmodule Stateful do
-    use Symbelix.Library
-    require Logger
-    alias SymbelixTest.Memory
-
-    def add(a, b), do: a + b
-
-    def set(name, value) do
-      :ok = Memory.set(Memory, name, value)
-      "ok"
-    end
-
-    def get(name) do
-      Memory.get(Memory, name)
-    end
-
-    def inc(name) do
-      set(name, get(name) + 1)
-    end
-  end
-
-  defmodule Memory do
-    def start_link do
-      Agent.start_link(fn -> %{} end)
-    end
-
-    def set(pid, key, value) do
-      Agent.update(pid, fn state -> Map.put(state, key, value) end)
-    end
-
-    def get(pid, key) do
-      Agent.get(pid, fn state -> Map.get(state, key) end)
-    end
-  end
+  alias Symbelix.TestHelpers.Libraries.{
+    Mathematician,
+    Controller,
+    Talker,
+    ListProcessor,
+    Java,
+    Stateful,
+    Memory
+  }
 
   doctest Symbelix
 
